a0b6f21f7f85229702345ea24214d02bc3c71241
jrnold <jeffrey.arnold@gmail.com>
2015-10-29 18:46:12 -0700

    bumping version

--- inst/scripts/download_agresti_csv.sh
+++ inst/scripts/download_agresti_csv.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wget -l 1 -r -A ../extdata -nd http://www.stat.ufl.edu/~aa/social/csv_files/ 
+wget -l 1 -r -A ../extdata -nd http://www.stat.ufl.edu/~aa/social/csv_files/
+# fl.crime csv needs to be manually edited to fix first column
